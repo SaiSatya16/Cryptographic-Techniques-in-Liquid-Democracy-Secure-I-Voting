c383bebc38614ccb099c9600152b5936554bdbb0
jj1039 <jj1039@srmist.edu.in>
2024-03-08 01:08:22 +0530

    Update voterhome.js

--- static/vue/components/voterhome.js
+++ static/vue/components/voterhome.js
@@ -1,10 +1,69 @@
 const Voterhome = Vue.component("adminhome", {
-    template:  `<div class="container mt-5">
-                    <div class="alert alert-info" role="alert">
-                        <h4 class="alert-heading">Page is coming soon, stay tuned!</h4>
-                        <p>We're working on something awesome. Please check back later.</p>
-                    </div>
-                </div>`,
+    template:  
+    `
+
+    
+    
+    `,
+    data () {
+        return {
+            userRole: localStorage.getItem("role"),
+            token: localStorage.getItem("auth-token"),
+            username: localStorage.getItem("username"),
+            user_id: localStorage.getItem("id"),
+            error: null,
+            schemes: [],
+            vote: null,
+        }
+    },
+    methods: {
+        async getschemes() {
+            const res = await fetch("/scheme/"+ this.user_id, {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                "Authentication-Token": this.token,
+                "Authentication-Role": this.userRole,
+              },
+            });
+            if (res.ok) {
+              const data = await res.json();
+              console.log(data);
+              this.schemes = data;
+            } else {
+              const data = await res.json();
+              console.log(data);
+              this.error = data.error_message;
+            }
+          },
+
+        async vote(scheme) {
+            const res = await fetch("/vote", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Authentication-Token": this.token,
+                "Authentication-Role": this.userRole,
+              },
+              body: JSON.stringify({
+                scheme_id: scheme.id,
+                user_id: this.user_id,
+                vote: this.vote,
+              }),
+            });
+            if (res.ok) {
+              this.getschemes();
+            } else {
+              const data = await res.json();
+              console.log(data);
+              this.error = data.error_message;
+            }
+          },
+
+
+}
+
+
 });
 
 export default Voterhome;
